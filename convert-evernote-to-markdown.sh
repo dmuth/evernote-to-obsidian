baa11e89ce39fb466d794d6c6c0e938ed137d1bf
Douglas Muth <dmuth@dmuth.org>
2021-03-28 18:12:44 -0400

    Added logic to move folders out of extra notes/ directory and deal with dupes.

--- convert-evernote-to-markdown.sh
+++ convert-evernote-to-markdown.sh
@@ -36,3 +36,45 @@ cat config.json.IN \
 
 yarle --configFile ./config.json
 
+#
+# This crazy logic here is to do the following:
+# 
+# 1) Move the various notebooks out of the notes/ directory and into the parent, because
+# I see not point to the notes/ directory existing.
+# 
+# 2) In an effort to make things more user-friendly, if this script has been run before,
+# simply add an ascending number to the end and try again, instead of having the script bail.
+#
+#
+echo
+echo
+echo "# Moving directories from ${TARGET}/notes/ into just ${TARGET}..."
+pushd ${TARGET} > /dev/null
+for FILE in notes/*
+do
+	DEST=$(basename "${FILE}")
+	if test -e "${DEST}"
+	then
+		I=1
+		DEST="${DEST}.${I}"
+		echo "# Checking for ${DEST}..."
+		while test -e "${DEST}"
+		do
+			echo "# Looks like ${DEST} already exists, let's increment and try again..."
+			I=$(( I += 1 ))
+			DEST=$(basename "${FILE}")
+			DEST="${DEST}.${I}"
+		done
+
+		echo "# Our new target will be ${DEST}!"
+	fi
+
+	echo "# Moving notes/${FILE} to ${DEST}..."
+	mv "${FILE}" "${DEST}"
+
+done
+
+rm -rf notes
+popd > /dev/null
+
+
